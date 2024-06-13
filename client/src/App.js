08438754c96de3930e872912c77c303ad388bc6d
srimanikantaBattu <srimanikantabattu04@gmail.com>
2024-06-13 22:24:38 +0530

    my commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import Home from './components/LandingPage'
 function App() {
   return (
-    <div className=''>
-      
+    <div className="">
+      <Home></Home>
     </div>
   )
 }
---
+++ client/src/components/LandingPage.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useState,useEffect } from 'react'
+import Video from '../assets/landVideo.mp4'
+function LandingPage() {
+
+
+  const text1 = "Uncover the Hidden Stories of Any Location with GeoChronicle AI";
+  const text2 = "Explore comprehensive historical insights and significant events of any location through the power of AI.";
+  const [displayText, setDisplayText] = useState('');
+  const [currentText, setCurrentText] = useState(text1);
+  const [charIndex, setCharIndex] = useState(0);
+  useEffect(() => {
+    if (charIndex < currentText.length) {
+      const timeoutId = setTimeout(() => {
+        setDisplayText((prev) => prev + currentText[charIndex]);
+        setCharIndex((prev) => prev + 1);
+      }, 100); // Delay between letters
+      return () => clearTimeout(timeoutId);
+    } else if (currentText === text1) {
+      setTimeout(() => {
+        setDisplayText(''); // Clear the text
+        setCurrentText(text2); // Switch to the second text
+        setCharIndex(0); // Reset index for new text
+      }, 2000); // Delay before starting the second text
+    }
+  }, [charIndex, currentText]);
+
+  return (
+    <div>
+        <video autoPlay muted loop src={Video}></video>
+        <div className="centered-text">
+        <p className='landpage-para'>GeoChronicle</p>
+        </div>
+        <div style={{width:"470px",marginRight:"6.3rem"}} className="centered-text mt-5">
+            <p style={{fontSize:"1.2rem"}} className='animated text-center'>{displayText}</p>
+        </div>
+    </div>
+  )
+}
+
+export default LandingPage
